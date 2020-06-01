098c501442da38b48ca5a15a535d345ed5a52c6d
ambikeshkumargautam <ambikeshkumargautam@cedcoss.com>
2020-06-01 10:38:08 +0530

    added side bar in function.php

--- functions.php
+++ functions.php
@@ -66,3 +66,21 @@ function register_my_menus() {
 }
 	add_action( 'init', 'register_my_menus' );
 
+/**
+ * Register our sidebars and widgetized areas.
+ */
+function arphabet_widgets_init() {
+
+	register_sidebar(
+		array(
+			'name'          => 'Right sidebar',
+			'id'            => 'right_sidebar',
+			'before_widget' => '<div>',
+			'after_widget'  => '</div>',
+			'before_title'  => '<h2 class="rounded">',
+			'after_title'   => '</h2>',
+		)
+	);
+
+}
+	add_action( 'widgets_init', 'arphabet_widgets_init' );
