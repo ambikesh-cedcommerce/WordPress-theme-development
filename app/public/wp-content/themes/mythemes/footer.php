7117b0f423535305a67cd277d1c9228be2851490
ambikeshkumargautam <ambikeshkumargautam@cedcoss.com>
2020-06-01 14:11:52 +0530

    rmeoved testtxt file and modified footer.php page

--- app/public/wp-content/themes/mythemes/footer.php
+++ app/public/wp-content/themes/mythemes/footer.php
@@ -1,7 +1,7 @@
 <!-- Footer -->
 	<footer class="py-5 bg-dark">
 		<div class="container">
-			<p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
+			<p class="m-0 text-center text-white">Copyright &copy; Your Website 2020</p>
 		</div>
 		<!-- /.container -->
 	</footer>
@@ -16,10 +16,10 @@
 	 */
 
 	?>
-<<<<<<< HEAD
+
 	<?php	wp_footer(); ?>
-=======
+
 	<?php	wp_footer(); ?>	
->>>>>>> 714282f345da79e9925054e568272941e6b7c150
+
 	</body>
 </html>
