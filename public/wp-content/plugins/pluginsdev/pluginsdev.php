902e68403f08815c7a2d97f3df9fdb3b44af1b18
ambikeshkumargautam <ambikeshkumargautam@cedcoss.com>
2020-06-15 12:32:19 +0530

    Added Boiler plugin demo and pluginsdev

---
+++ public/wp-content/plugins/pluginsdev/pluginsdev.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Myplugins-dev
+ *
+ * @package           Pluginsdev
+ * @author            Ambikesh kumar Gautam
+ * @copyright         2020 Cedcoss PVT LKO
+ * @license           GPL-2.0-or-later
+ */
+
+/**
+ * Myplugins-dev
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Pluginsdev
+ * Plugin URI:        https://github.com/ambikeshkumargautam-cedcoss/WordPress-theme-development/tree/master/public/wp-content/plugins/myplugins-dev
+ * Description:       This Plugins is about making our own plugins in WordPress.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.3
+ * Author:            Ambikesh Kumar Gautam
+ * Author URI:        https://github.com/ambikeshkumargautam-cedcoss
+ * Text Domain:       plugin-slug
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+/*
+{Plugin Name} is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+{Plugin Name} is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with {Plugin Name}. If not, see {License URI}.
+*/
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ */
+define( 'PLUGIN_NAME_VERSION', '1.0.0' );
