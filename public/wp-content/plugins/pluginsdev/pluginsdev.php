f7fd47050655efd34254dc26634fc597c10f109b
ambikeshkumargautam <ambikeshkumargautam@cedcoss.com>
2020-06-15 14:16:08 +0530

    Added activation and deativation hooks and require core plugins class

--- public/wp-content/plugins/pluginsdev/pluginsdev.php
+++ public/wp-content/plugins/pluginsdev/pluginsdev.php
@@ -49,3 +49,27 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  */
 define( 'PLUGINSDEV_VERSION', '1.0.0' );
+/**
+ * The will activate a plugins and enable new custom post in the WP_Admin .
+ * This action is documented in includes/class-pluginsdev-activator.php
+ */
+function activate_pluginsdev() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-pluginsdev_activator.php';
+	plugins_Activator::activate(); // Allows access to method (activate) of class Plugins_Activator .
+}
+
+/**
+ * This function will deactivate this pluginsdev .
+ * This action is documented in includes/class-pluginsdev-deactivator.php.
+ */
+function deactivate_pluginsdev() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-pluginsdev-deactivator.php';
+	plugins_Deactivator::deactivate(); // Allows access to method (deactivate) of class Plugins_Deactivator .
+}
+register_activation_hook( __FILE__, 'activate_pluinsdev' ); // This will activate this function which we want to activate in the plugins functionality.
+register_deactivation_hook( __FILE__, 'deactivate_pluginsdev' ); // This will deactivate all the custom post or feature which we want to remove after the deactivation of the plugins .
+
+/**
+ * The core  pluginsdev class that is used to defive internationalization ,
+ * admin-specific hooks , and public-facing site hooks.
+ */
