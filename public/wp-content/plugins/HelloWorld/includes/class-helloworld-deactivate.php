9c4d6f65c04168ab1e5c5ad79bd7c0a1e505e561
ambikeshkumargautam <ambikeshkumargautam@cedcoss.com>
2020-06-17 00:46:44 +0530

    rename folder name and class-HelloWorld->class-helloworld

--- public/wp-content/plugins/HelloWorld/includes/class-helloworld-deactivate.php
+++ public/wp-content/plugins/HelloWorld/includes/class-helloworld-deactivate.php
@@ -1,12 +1,15 @@
 <?php
+/**
+ * This is doc somment .
+ *
+ *  @package helloWorld .
+ */
 
 /**
  * This is activation hook after activate  of plugin .
  *
  * @package pluginsdev
  */
-
-
 class HelloWorld_Deactivate {
 	/**
 	 * This function will activation for the plugins
